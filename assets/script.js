f2d068351758c84884008e5f01a1bf8707177a69
aziz <mraziz.aden@gmail.com>
2023-06-27 13:53:58 -0400

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var startBtnEl = document.getElementById("startBtn");
+var questionEl = document.getElementById("question");
+var startEl = document.getElementById("start");
+var timeEl = document.getElementById('timeLeft');
+var answerEls = document.querySelectorAll('.choices a');
+var score = 0;
+var questionsAndAnswers = [
+  {
+ 
